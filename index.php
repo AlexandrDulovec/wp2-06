2e754f77be916655095363d07d6b68faa5686165
AlexandrDulovec <79139997+AlexandrDulovec@users.noreply.github.com>
2022-01-05 13:46:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,37 +9,6 @@
 <body>
 <?php
 // Lidé a filmy //
-$people[0]['firstname'] = "Jakub";
-$people[0]['lastname'] = "Vlček";
-$people[0]['age'] = 30;
-$people[0]['height'] = 181;
-$people[0]['weight'] = 83;
-
-$people[1]['firstname'] = "Martin";
-$people[1]['lastname'] = "Fenin";
-$people[1]['age'] = 34;
-$people[1]['height'] = 180;
-$people[1]['weight'] = 81;
-
-$people[2]['firstname'] = "Karel";
-$people[2]['lastname'] = "Gott";
-$people[2]['age'] = 80;
-$people[2]['height'] = 176;
-$people[2]['weight'] = 70;
-
-$people[3]['firstname'] = "Jakub";
-$people[3]['lastname'] = "Janeček";
-$people[3]['age'] = 35;
-$people[3]['height'] = 185;
-$people[3]['weight'] = 68;
-
-$people[4]['firstname'] = "Daniel";
-$people[4]['lastname'] = "Vávra";
-$people[4]['age'] = 46;
-$people[4]['height'] = 169;
-$people[4]['weight'] = 90;
-
-
 $movies[0]['name'] = "Tenkrát v Hollywoodu";
 $movies[0]['director'] = "Quentin Tarantino";
 $movies[0]['year'] = "2019";
@@ -64,7 +33,44 @@ $movies[4]['name'] = "8. míle";
 $movies[4]['director'] = "Curtis Hanson";
 $movies[4]['year'] = "2003";
 $movies[4]['genre'] = "Drama";
+    
+    
+$people[0]['firstname'] = "Jakub";
+$people[0]['lastname'] = "Vlček";
+$people[0]['age'] = 30;
+$people[0]['height'] = 181;
+$people[0]['weight'] = 83;
+$people[0['movie'] = movies[1]; 
 
+$people[1]['firstname'] = "Martin";
+$people[1]['lastname'] = "Fenin";
+$people[1]['age'] = 34;
+$people[1]['height'] = 180;
+$people[1]['weight'] = 81;
+$people[1]['movie'] = movies[3];
+
+$people[2]['firstname'] = "Karel";
+$people[2]['lastname'] = "Gott";
+$people[2]['age'] = 80;
+$people[2]['height'] = 176;
+$people[2]['weight'] = 70;        
+$people[2]['movie'] = movies[2];
+
+$people[3]['firstname'] = "Jakub";
+$people[3]['lastname'] = "Janeček";
+$people[3]['age'] = 35;
+$people[3]['height'] = 185;
+$people[3]['weight'] = 68;
+$people[3]['movie'] = movies[4];
+
+$people[4]['firstname'] = "Daniel";
+$people[4]['lastname'] = "Vávra";
+$people[4]['age'] = 46;
+$people[4]['height'] = 169;
+$people[4]['weight'] = 90;    
+$people[4]['movie'] = movies[5]; 
+    
+    
 for ($i=0; $i <count($people); $i++) { ?>{ 
     <h2><?= $people[$i]['name'];?></h2>
     <p><strong>Jméno: </strong><?= $people[$i]['name'];?></p>
@@ -72,14 +78,8 @@ for ($i=0; $i <count($people); $i++) { ?>{
     <p><strong>Věk: </strong><?= $people[$i]['age'];?></p>
     <p><strong>Výška: </strong><?= $people[$i]['height'];?></p> 
     <p><strong>Váha: </strong><?= $people[$i]['weight'];?></p>
-}
+    <p><strong>Nejoblíbenější film: </strong><?= $people[$i]['movie'];?></p>
 
-for ($i=0; $i < count($movies); $i++) { ?>
-    <h2><?= $movies[$i]['name'];?></h2>
-    <p><strong>Jméno: </strong><?= $movies[$i]['name'];?></p>
-    <p><strong>Režisér: </strong><?= $movies[$i]['director'];?></p>
-    <p><strong>Rok vydání: </strong><?= $movies[$i]['year'];?></p>
-    <p><strong>Žánr: </strong><?= $movies[$i]['genre'];?></p>
 
 <?php
 }
